3f41fc548b8240738f41c04711da0396b5f8ec82
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2025-02-25 03:07:11 +0530

    Basic layout done

---
+++ server/Controllers/AuthControllers.js
@@ -0,0 +1,7 @@
+export const register = async (req, res, next) => {
+    res.json({ message: "Register function executed!" });
+};
+
+export const login = async (req, res, next) => {
+    res.json({ message: "Login function executed!" });
+};
--- server/index.js
+++ server/index.js
@@ -1,7 +1,39 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import AuthRoutes from "./Routes/AuthRoutes.js";
 const app = express();
+const uri = "mongodb+srv://diyabala:db03082024@cluster0.5hj7ajf.mongodb.net/"; 
 
 const PORT = 4000;
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
 })
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to MongoDB Atlas");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
+
+connectDB();
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+        credentials: true
+    })
+)
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/", AuthRoutes);
